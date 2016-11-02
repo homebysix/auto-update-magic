9e1aca1307ff0922297e7f253404d8ddae502919
Elliot Jordan <elliot@elliotjordan.com>
2016-11-02 16:52:45 -0700

    Changed com.jamfsoftware to com.jamf to reflect new branding.
    Closes #30.

--- Exercise5c/create_pkg.sh
+++ Exercise5c/create_pkg.sh
@@ -8,25 +8,27 @@
 #                   pair to your managed clients.
 #          Author:  Elliot Jordan <elliot@lindegroup.com>
 #         Created:  2015-09-18
-#   Last Modified:  2015-09-23
-#         Version:  1.0.1
+#   Last Modified:  2016-11-02
+#         Version:  1.0.2
 #
 ###
 
 cd "$(dirname "$0")"
 
 if [[ ! -f "./auto_update_magic.sh" ||
-      ! -f "./com.jamfsoftware.jamfnation.auto_update_magic.plist" ||
+      ! -f "./com.jamf.jamfnation.auto_update_magic.plist" ||
+      ! -f "./pkg_scripts/preinstall" ||
       ! -f "./pkg_scripts/postinstall" ]]; then
     echo "[ERROR] At least one required file is missing. Ensure that the following files exist in the Exercise5c folder:"
     echo "    auto_update_magic.sh"
-    echo "    com.jamfsoftware.jamfnation.auto_update_magic.plist"
+    echo "    com.jamf.jamfnation.auto_update_magic.plist"
+    echo "    pkg_scripts/preinstall"
     echo "    pkg_scripts/postinstall"
     exit 1
 fi
 
 script_md5=$(md5 -q ./auto_update_magic.sh)
-if [[ "$script_md5" == "a1fd08b32853ca004212e98d7ea5a99e" ]]; then
+if [[ "$script_md5" == "9cc45872b24b1126f5c6cc9eaa3f9a44" ]]; then
     echo "[ERROR] It looks like you haven't customized the auto_update_magic.sh script yet. Please do that now, then run create_pkg.sh again."
     exit 2
 fi
@@ -45,7 +47,7 @@ if [[ "$check_blocking_apps" != "y" && "$check_blocking_apps" != "Y" ]]; then
     exit 4
 fi
 
-read -p "[SANITY CHECK] Have you already adjusted the StartInterval in the com.jamfsoftware.jamfnation.auto_update_magic LaunchDaemon to your liking? [y/n]: " -n 1 check_schedule
+read -p "[SANITY CHECK] Have you already adjusted the StartInterval in the com.jamf.jamfnation.auto_update_magic LaunchDaemon to your liking? [y/n]: " -n 1 check_schedule
 echo
 if [[ "$check_schedule" != "y" && "$check_schedule" != "Y" ]]; then
     echo "You should go back and adjust the StartInterval now, then run create_pkg.sh again."
@@ -59,7 +61,7 @@ echo "Building package root in /tmp folder..."
 mkdir -p "$TMP_PKGROOT/Library/LaunchDaemons" "$TMP_PKGROOT/Library/Scripts"
 
 echo "Copying the files to the package root..."
-cp "./com.jamfsoftware.jamfnation.auto_update_magic.plist" "$TMP_PKGROOT/Library/LaunchDaemons/"
+cp "./com.jamf.jamfnation.auto_update_magic.plist" "$TMP_PKGROOT/Library/LaunchDaemons/"
 cp "./auto_update_magic.sh" "$TMP_PKGROOT/Library/Scripts/"
 
 echo "Setting mode and permissions..."
@@ -69,7 +71,7 @@ chmod +x "$TMP_PKGROOT/Library/Scripts/auto_update_magic.sh"
 echo "Building the package..."
 pkgbuild --root "/tmp/auto_update_magic/pkgroot" \
          --scripts "./pkg_scripts" \
-         --identifier "com.jamfsoftware.jamfnation.auto_update_magic" \
+         --identifier "com.jamf.jamfnation.auto_update_magic" \
          --version "2.2" \
          --install-location "/" \
          "./auto_update_magic-$(date "+%Y%m%d").pkg"
